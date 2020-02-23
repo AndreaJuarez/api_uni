34acf4e3272a36d3f97cd0c7c5a59ae149e72ddd
Andrea Juarez <1718110394@utectulancingo.edu.mx>
2020-02-23 03:15:40 -0600

    Version uno funcionando

---
+++ app.py
@@ -0,0 +1,18 @@
+# TODO buscar comandos de git en consola de visual, git con linea de comandos 
+# TODO ingresar el metodo para que salga el error
+
+import web
+
+from web.template import ALLOWED_AST_NODES
+ALLOWED_AST_NODES.append('Constant')
+
+urls = (
+	'/alumnos?', 'application.controllers.alumnos.Alumnos' #el simbolo ? inidca que recibira variables en la URL
+)
+app = web.application(urls, globals())
+
+#render = web.template.render('templates/')  #dice que la carpeta de las paginas web sera templates
+
+if __name__ == "__main__":
+	web.config.debug = False
+	app.run()
