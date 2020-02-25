40dbc4f3ba16fbba7c31bfdbaf4378bfdadfd547
AndreaJuarez <1718110394@utectulancingo.edu.mx>
2020-02-25 10:47:45 -0600

    prueba

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 import web
 
 from web.template import ALLOWED_AST_NODES
-ALLOWED_AST_NODES.append('Constant') bjcbsjcb
+ALLOWED_AST_NODES.append('Constant') bjc
 
 urls = (
 	'/alumnos?', 'application.controllers.alumnos.Alumnos' #el simbolo ? inidca que recibira variables en la URL
