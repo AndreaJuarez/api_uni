509609a65aa21dd37bbf405fa3f9ba199d9900bf
AndreaJuarez <1718110394@utectulancingo.edu.mx>
2020-02-26 23:02:08 -0600

    version 0.2.0 funcionando

--- application/controllers/alumnos.py
+++ application/controllers/alumnos.py
@@ -10,29 +10,58 @@ class Alumnos:
         try:
             datos=web.input()     #Los datos introducidos por el usuario se almacenaran en datos
             if datos['token']=="1234":     #Si el usuario ingresa bien el token se declarara lo siguiente
-                result=[]           #Un arreglo
-                result2={}          #Un diccionario
                 if datos['action']=="get":        #Si accion es get va a hacer lo siguiente
+                    result=[]           #Un arreglo
+                    result2={}          #Un diccionario
+                    result2['app_version']="0.2.0" #visualizacion de datos lista
+                    result2['status']="200 OK"
                     with open('static/csv/alumnos.csv','r') as csvfile:   #Ruta del archivo csv que va a leer, r es de lectura, csvfile es una variable cualquiera
                         reader = csv.DictReader(csvfile)         #Lector del archivo, DictReader te almacena los datos como en diccionario en este caso en la variable reader
                         for row in reader:              #Lee la primer fila y la manda la arreglo
                             result.append(row)          #Lo manda al arreglo result
-                            result2['app_version']="0.1.0" #visualizacion de datos lista
-                            result2['status']="200 OK"
                             result2['alumnos']=result      #Result2 en la posicion alumnos, sera lo que va a almacenar en result
                     return json.dumps(result2)          #Va a regresar un json del result2 que es lo que va almacenando el arreglo
-                else:                           #Si accion no es get va a poner comando no encontrado
-                    result2={}
-                    result2['app_version']="0.1.0" #visualizacion de datos lista
-                    result2['status']="Command not found"
-                    return json.dumps(result2)
+                
+                if datos['action']=="insert":
+                    result={} 
+                    result['app_version']="0.2.0" 
+                    result['status']="200 OK"
+                    matricula = str(datos['matricula'])
+                    nombre = str(datos['nombre'])
+                    primer_apellido = str(datos['primer_apellido'])
+                    segundo_apellido = str(datos['segundo_apellido'])
+                    carrera = str(datos['carrera'])
+
+                    result = [] #Crea array
+                    result.append(matricula)
+                    result.append(nombre)
+                    result.append(primer_apellido)
+                    result.append(segundo_apellido)
+                    result.append(carrera)
+
+                    resultado={}
+                    resultado['matricula']=matricula
+                    resultado['nombre']=nombre
+                    resultado['primer_apellido']=primer_apellido
+                    resultado['segundo_apellido']=segundo_apellido
+                    resultado['carrera']=carrera
+                    
+                    result2=[]
+                    result2.append(resultado)
+
+                    with open('static/csv/alumnos.csv', 'a+', newline='') as variable_cualquiera:
+                        writer = csv.writer(variable_cualquiera) 
+                        writer.writerow(result)
+                    return result
+
             else:
                 result={}
-                result['app_version']="0.1.0" #visualizacion de datos lista
+                result['app_version']="0.2.0" #visualizacion de datos lista
                 result['status']="Los datos insertados son incorrectos"
                 return json.dumps(result)
-        except Exception:
+
+        except Exception as e:
             result={}
-            result['app_version']="0.1.0" #visualizacion de datos lista
-            result['status']="Faltan valores por insertar"
+            result['app_version']="0.2.0" #visualizacion de datos lista
+            result['status']=("Faltan valores por insertar{}".format(e.args))
             return json.dumps(result)
